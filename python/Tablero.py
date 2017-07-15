d57218743d7bb9527deeafd0675848844015ee94
marcos mayen <marcosdmayen@gmail.com>
2017-07-15 16:59:12 -0600

    rec

--- python/Tablero.py
+++ python/Tablero.py
@@ -1,4 +1,4 @@
-from nodoMatriz import nodoMatriz
+from nodoTablero import nodoTablero
 
 class Tablero():
 	def __init__(self):
@@ -6,157 +6,98 @@ class Tablero():
 		self.fila=0
 		self.columna=0
 
-	def crear(self,x,y):
-		base_y=None
-		mueve=None
-		base=None
-		mueveY=None
-		mueveX=None
+	def battlefield(self,fila,columna):
+		self.fila=fila
+		self.columna=columna
+		self.tablero=None
 		x=0
 		y=0
 		z=0
-
-
-
-		self.fila=x
-		self.columna=y
-		fila=0
-		columna=0
+		base_y=None
+		raiz=None
 		mueve=None
-		temporal=None
-		inicio=None
-		previo=None
-		up_left=None
-		while fila<x:
-			while columna<y:
-				if self.tablero==None:
-					self.tablero=nodoMatriz(0,None,None,None,None,None,None)
-					temporal=self.tablero
-					mueve=self.tablero
-					inicio=self.tablero
-					abajo=self.tablero
-					# UP_DOWN
-					for x in range (0,3):
-						sube=nodoMatriz(0,None,None,None,None,None,None)
-						abajo.up=sube
-						sube.down=abajo
-						abajo=abajo.up
-				elif fila==0:
-					#UP_DOWN... FORWARD_BACK
-					abajo=nodoMatriz(0,None,None,None,None,None,None)
-					abajo.back=mueve
-					mueve.forward=abajo
-					tmp=mueve
-					mueve=abajo
-					for x in range(0,3):
-						sube=nodoMatriz(0,None,None,None,None,None,None)
-						abajo.up=sube
-						sube.down=abajo
-						abajo=abajo.up
-						tmp=tmp.up
-						tmp.forward=abajo
-						abajo.back=tmp
-				else:
-					abajo=nodoMatriz(0,None,None,None,None,None,None)
-					abajo.left=temporal
-					temporal.right=abajo
-					up_left=temporal
-					if columna==0:
-					# UP_DOWN...LEFT_RIGHT
-						previo=abajo
-						for x in range (0,3):
-							up_left=up_left.up
-							sube=nodoMatriz(0,None,None,None,None,None,None)
-							abajo.up=sube
-							sube.down=abajo
-							abajo=abajo.up
-							abajo.left=up_left
-							up_left.right=abajo
+		mueveY=None
+		base=None
+		mueve=None
+		while x<fila:
+			while y<columna:
+				while z<4:
+					casilla = nodoTablero("0",None,None,None,None,None,None)
+					if(self.tablero==None):
+						self.tablero=casilla
+						base=self.tablero
+						mueve=self.tablero
+						mueve=self.tablero
+						mueveY=self.tablero
+						base_y=self.tablero
 					else:
-						# UP_DOWN...LEFT_RIGHT...FORWARD_BACK
-						prv=previo
-						previo=previo.forward
-						for x in range (0,3):
-							up_left=up_left.up
-							sube=nodoMatriz(0,None,None,None,None,None,None)
-							abajo.up=sube
-							sube.down=abajo
-							abajo=abajo.up
-							abajo.left=up_left
-							up_left.right=abajo
-							prv.forward=abajo
-							abajo.back=prv
+						if y==0 and x==0:
+							mueve.up=casilla
+							casilla.down=mueve
+							mueve=mueve.up
 
-					temporal=temporal.forward
-				columna=columna+1
-			columna=0
-			temporal=inicio
-			inicio=inicio.right
-			fila=fila+1
+						elif y==0:
+							if z==0:
+								mueve.frente=casilla
+								casilla.tras=mueve
+								mueve=mueve.frente
+								base=mueve
+								mueveY=base
+							else:
+								casilla.down=mueve
+								mueve.up=casilla
+								mueve=mueve.up
+								mueve=mueve.up
+								mueve.tras=mueve
+								mueve.frente=mueve
+						elif x==0:
+							if z==0:
+								mueve.der=casilla
+								casilla.izq=mueve
+								mueve=mueve.der
+							else:
+								casilla.down=mueve
+								mueve.up=casilla
+								mueve=mueve.up
+								mueve=mueve.up
+								mueve.izq=mueve
+								mueve.der=mueve
+						else:
+							if z==0:
+								mueve=base_y
+								mueve=mueve.der
+								mueveY=base_y
+								mueve.der=casilla
+								casilla.izq=mueve
+								mueve=mueve.der
+								base_y=mueve
+							else:
+								# mueve=mueve.up
+								mueveY=mueveY.up
+								mueve.up=casilla
+								casilla.down=mueve
+								mueve=mueve.up
+								mueveY.der=mueve
+								mueve.izq=mueveY
+
+							mueve.frente=mueve
+							mueve.tras=mueve	
+
+					z=z+1
+				z=0
+				y=y+1
+			mueve=base
+			mueve=base
+			mueveY=base
+			base_y=base
+			y=0
+			x=x+1
+
 		return "v"
 
-	def verTablero(self):
-		if self.tablero.right!=None:
-			if self.tablero.forward!=None:
-				# quiere decir que no es un misero arreglo de 1x1x4
-				fila=self.tablero.right
-				columna=fila
-				cadena=""
-				x=0
-				y=0
-				while fila:
-					while columna:
-						for z in range(0,3):
-							cadena=cadena+"\""
-						columna=columna.forward
-					fila=fila.right
-					columna=fila
-					columna=columna+1
-					y=x
-					x=x.right
-			else:
-				# es un arreglode nx1x4(practicamente una matriz)
-		else:
-			if self.tablero.forward!=None:
-				# es una matriz
-			else:
-				# es un inche vector
+	def ver_matriz(self):
+		return self.tablero.up.valor
 
 
-
-
-				if(fila==0)
-		while fila<self.fila:
-			while columna<self.columna:
-				if fila==0:
-					if columna==0:
-						for x in xrange(0,3):
-							# UP_DOWN
-							cadena=cadena+"\""+fila","+columna+","+str(int(x+1))+"_"+tmp.up.ocupado+"\"->\""+fila","+columna+","+x+"_"tmp+"\";\n"
-							cadena=cadena+"\""+fila","+columna+","+x+"_"+tmp+"\"->\""+fila","+columna+","+str(int(x+1))+"_"tmp.up.ocupado+"\";\n"
-					else:
-						# LEFT_RIGHT
-						cadena=cadena+"\""+fila","+columna+","+str(int(x+1))+"_"+tmp.up.ocupado+"\"->\""+fila","+columna+","+x+"_"tmp+"\";\n"
-						cadena=cadena+"\""+fila","+columna+","+x+"_"+tmp+"\"->\""+fila","+columna+","+str(int(x+1))+"_"tmp.up.ocupado+"\";\n"
-						for x in xrange(0,3):
-							# UP_DOWN
-							cadena=cadena+"\""+fila","+columna+","+str(int(x+1))+"_"+tmp.up.ocupado+"\"->\""+fila","+columna+","+x+"_"tmp+"\";\n"
-							cadena=cadena+"\""+fila","+columna+","+x+"_"+tmp+"\"->\""+fila","+columna+","+str(int(x+1))+"_"tmp.up.ocupado+"\";\n"
-							# FORWARD_BACK
-							cadena=cadena+"\""+fila","+columna+","+str(int(x+1))+"_"+tmp.up.ocupado+"\"->\""+fila","+columna+","+x+"_"tmp+"\";\n"
-							cadena=cadena+"\""+fila","+columna+","+x+"_"+tmp+"\"->\""+fila","+columna+","+str(int(x+1))+"_"tmp.up.ocupado+"\";\n"
-				elif columna==0:
-
-
-				columna=columna+1
-				tmp=tmp.forward
-			columna=0
-			tmp
-			fila=fila+1
-		while :
-			pass
-
-	def cargar_nave(self,fila,columna,nivel,modo,direccion):
-		return "true"
 
 		
---
+++ python/nodoTablero.py
@@ -0,0 +1,10 @@
+class nodoTablero:
+	def __init__(self,valor=None,up=None,down=None,izq=None,
+		der=None,frente=None,tras=None):
+		self.valor=valor
+		self.up=up
+		self.down=down
+		self.izq=izq
+		self.der=der
+		self.frente=frente
+		self.tras=tras
